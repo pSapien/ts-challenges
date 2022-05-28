91f1b57efd9067c83af157baf2091c872cce19e4
psapien <prateekthapa26@gmail.com>
2022-05-28 17:01:31 +0545

    easy-ifs

---
+++ easy/00268-easy-if.ts
@@ -0,0 +1,17 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "../test-utils";
+
+type cases = [
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
+];
+
+// @ts-expect-error
+type error = If<null, "a", "b">;
+
+// ============= Your Code Here =============
+
+// prettier-ignore
+type If<C, T, F> = C extends null
+  ? never
+  : C extends true ? T : F
