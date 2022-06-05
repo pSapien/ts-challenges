4e7f0d003918759e29318d27917baa696a8aa3d3
psapien <prateekthapa26@gmail.com>
2022-06-05 10:09:28 +0545

    00012-medium-chainable-options

--- medium/00009-medium-deep-readonly.ts
+++ medium/00009-medium-deep-readonly.ts
@@ -1,5 +1,5 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from "./test-utils";
+import type { Equal, Expect } from "../test-utils";
 
 type cases = [Expect<Equal<DeepReadonly<X>, Expected>>];
 
